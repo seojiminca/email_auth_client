21e3d4de2b03fc4b6be9078acbbcacbe34ef501c
jimin <seojiminca@gmail.com>
2020-08-07 16:25:02 +0900

    add register style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Register from "./screens/Register";
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
       <Switch>
           <Route path='/' exact render={props => <App {...props}/>} />
+          <Route path='/register' exact render={props => <Register {...props} />} />
       </Switch>
   </BrowserRouter>,
   document.getElementById('root')
