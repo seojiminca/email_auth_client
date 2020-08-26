0cfa13f5e74793152785bf66a119c793997fa595
jimin <seojiminca@gmail.com>
2020-08-26 11:15:55 +0900

    add login UI

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Register from "./screens/Register";
 import Activate from "./screens/Activate";
+import Login from "./screens/Login";
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import 'react-toastify/dist/ReactToastify.css';
@@ -12,7 +13,8 @@ ReactDOM.render(
       <Switch>
           <Route path='/' exact render={props => <App {...props}/>} />
           <Route path='/register' exact render={props => <Register {...props} />} />
-          <Route patt='/activate/:token' exact render={props => <Activate {...props} />} />
+          <Route path='/activate/:token' exact render={props => <Activate {...props} />} />
+          <Route path='/login' exact render={props => <Login {...props} />} />
       </Switch>
   </BrowserRouter>,
   document.getElementById('root')
